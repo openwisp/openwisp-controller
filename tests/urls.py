368c2c011690f6a59ebae402043de44cdae13cb7
Federico Capoano <nemesis@ninux.org>
2017-01-03 18:02:24 +0100

    [qa] Cleanup of flake8 and isort warnings

--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
 
 admin.autodiscover()
 
