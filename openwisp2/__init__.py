3d7375b171d07485661c684be0f6a08839cd3c3e
Federico Capoano <nemesis@ninux.org>
2017-01-03 18:20:46 +0100

    [setup] Updated get_version utility
    Ensure versioning follows PEP440
    https://www.python.org/dev/peps/pep-0440/

--- openwisp2/__init__.py
+++ openwisp2/__init__.py
@@ -3,14 +3,14 @@ __version__ = VERSION  # alias
 
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = '%s %s' % (version, VERSION[3])
+    version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
+    if VERSION[3] != 'final':
+        first_letter = VERSION[3][0:1]
+        try:
+            suffix = VERSION[4]
+        except IndexError:
+            suffix = 0
+        version = '%s%s%s' % (version, first_letter, suffix)
     return version
 
 
