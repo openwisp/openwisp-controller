ef4029fa075782619df2ceb9b3911c1bf5c622b6
Federico Capoano <nemesis@ninux.org>
2017-02-13 19:18:53 +0100

    [controller] Avoid repetition of URLs

--- openwisp2/config/controller/urls.py
+++ openwisp2/config/controller/urls.py
@@ -1,18 +1,4 @@
-from django.conf.urls import url
-
+from django_netjsonconfig.utils import get_controller_urls
 from . import views
 
-urlpatterns = [
-    url(r'^controller/checksum/(?P<pk>[^/]+)/$',
-        views.checksum,
-        name='checksum'),
-    url(r'^controller/download-config/(?P<pk>[^/]+)/$',
-        views.download_config,
-        name='download_config'),
-    url(r'^controller/report-status/(?P<pk>[^/]+)/$',
-        views.report_status,
-        name='report_status'),
-    url(r'^controller/register/$',
-        views.register,
-        name='register'),
-]
+urlpatterns = get_controller_urls(views)
