5e1bfed7bf2e8bb94c4866de0238a758d7ef5683
Federico Capoano <nemesis@ninux.org>
2017-02-23 17:40:50 +0100

    [users] Improved AppConfig of users app

---
+++ openwisp2/users/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'openwisp2.users.apps.UsersConfig'
--- openwisp2/users/apps.py
+++ openwisp2/users/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class UsersConfig(AppConfig):
-    name = 'users'
+    name = 'openwisp2.users'
+    app_label = 'users'
+    verbose_name = _('Users and Organizations')
