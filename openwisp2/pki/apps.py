15b9010b9361abbeed54afb18431fb34dd39fab0
Federico Capoano <nemesis@ninux.org>
2017-02-23 17:41:10 +0100

    [pki] Improved verbose_name

--- openwisp2/pki/apps.py
+++ openwisp2/pki/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class PkiConfig(AppConfig):
     name = 'openwisp2.pki'
-    verbose_name = 'PKI'
+    verbose_name = _('Public Key Infrastructure')
