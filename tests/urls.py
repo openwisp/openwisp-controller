161c6d430d2c6e2983b698daf475d5d79bb3a6f6
Federico Capoano <nemesis@ninux.org>
2017-03-10 12:03:20 +0100

    [tests] Added redirect from / to /admin in test env

--- tests/urls.py
+++ tests/urls.py
@@ -1,14 +1,19 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 from django_netjsonconfig.admin_theme.admin import admin, openwisp_admin
 
 openwisp_admin()
 
+redirect_view = RedirectView.as_view(url=reverse_lazy('admin:index'))
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('openwisp_controller.urls')),
+    url(r'^$', redirect_view, name='index')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
