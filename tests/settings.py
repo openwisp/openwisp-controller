da147f7803da9e17f518ca3297939335a9a300de
Federico Capoano <nemesis@ninux.org>
2017-03-15 17:48:47 +0100

    [test] Load openwisp2 admin theme earlier

--- tests/settings.py
+++ tests/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # openwisp2 admin theme
+    # (must be loaded here)
+    'django_netjsonconfig.admin_theme',
     # all-auth
     'django.contrib.sites',
     'allauth',
@@ -32,7 +35,6 @@ INSTALLED_APPS = [
     'openwisp_controller.pki',
     'openwisp_controller.config',
     # admin
-    'django_netjsonconfig.admin_theme',
     'django.contrib.admin',
     # other dependencies
     'sortedm2m',
