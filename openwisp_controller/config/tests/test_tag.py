26f3eb6070b935686250b0e2a8fa0bb748aac7cb
Federico Capoano <nemesis@ninux.org>
2017-04-26 17:41:13 +0200

    [feature] Added support for template tags #3
    Closes #3

---
+++ openwisp_controller/config/tests/test_tag.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from django_netjsonconfig.tests import CreateTemplateMixin
+from openwisp_users.tests.utils import TestOrganizationMixin
+
+from ..models import Template
+
+
+class TestTag(TestOrganizationMixin, CreateTemplateMixin, TestCase):
+    """
+    tests for Tag model
+    """
+    template_model = Template
+
+    def test_tag(self):
+        t = self._create_template(organization=self._create_org())
+        t.tags.add('mesh')
+        self.assertEqual(t.tags.filter(name='mesh').count(), 1)
