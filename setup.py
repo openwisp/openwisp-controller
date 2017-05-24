6f55826f353e56fa0b313c35c0422481ef40d803
Federico Capoano <nemesis@ninux.org>
2017-05-24 22:41:40 +0200

    [packaging] Updated publish command in setup.py script

--- setup.py
+++ setup.py
@@ -24,8 +24,9 @@ def get_install_requires():
 if sys.argv[-1] == 'publish':
     # delete any *.pyc, *.pyo and __pycache__
     os.system('find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf')
-    os.system('rm -rf build/')
-    os.system("python setup.py sdist bdist_wheel upload -s")
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload -s dist/*")
+    os.system("rm -rf dist build")
     args = {'version': get_version()}
     print("You probably want to also tag the version now:")
     print("  git tag -a %(version)s -m 'version %(version)s'" % args)
