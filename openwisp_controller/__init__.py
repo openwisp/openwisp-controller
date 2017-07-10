c4c9ba2c8b1c20c010e455a8aeb9345c928e5b2c
Federico Capoano <nemesis@ninux.org>
2017-07-10 18:15:16 +0200

    [version] Improved get_version to follow PEP440

--- openwisp_controller/__init__.py
+++ openwisp_controller/__init__.py
@@ -3,12 +3,16 @@ __version__ = VERSION  # alias
 
 
 def get_version():
-    version = '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
-    if VERSION[3] != 'final':
-        first_letter = VERSION[3][0:1]
-        try:
-            suffix = VERSION[4]
-        except IndexError:
-            suffix = 0
-        version = '%s%s%s' % (version, first_letter, suffix)
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if VERSION[3] != 'final':
+            try:
+                rev = VERSION[4]
+            except IndexError:
+                rev = 0
+            version = '%s%s%s' % (version, VERSION[3][0:1], rev)
     return version
