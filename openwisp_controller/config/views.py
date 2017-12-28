b050af8ef6a3e9efb491086696fdbc9407e78a92
AZtheAsian <alanyzhu@gmail.com>
2017-12-28 16:41:38 -0700

    [travis] Added django 2.0 to travis tests

--- openwisp_controller/config/views.py
+++ openwisp_controller/config/views.py
@@ -12,7 +12,10 @@ def get_default_templates(request, organization_id):
     returns default templates of specified organization
     """
     user = request.user
-    if not user.is_authenticated() and not user.is_staff:
+    authenticated = user.is_authenticated
+    if callable(authenticated):
+        authenticated = authenticated()
+    if not authenticated and not user.is_staff:
         return HttpResponse(status=403)
     org = get_object_or_404(Organization, pk=organization_id, is_active=True)
     templates = get_default_templates_queryset(org.pk, model=Template).only('id')
