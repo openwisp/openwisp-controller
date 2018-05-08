8d2af620d058dd362863168f2d0e87cf2e5f4bd0
Federico Capoano <nemesis@ninux.org>
2018-05-08 20:10:29 +0200

    [connections] Keep connector class and model logic decoupled

--- openwisp_controller/connection/tasks.py
+++ openwisp_controller/connection/tasks.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-from celery import shared_task
 from time import sleep
+
+from celery import shared_task
 
 from ..config.models import Device
 
