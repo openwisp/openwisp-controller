df755e330a92fd614a84ba519739db66c0f47fc9
Federico Capoano <nemesis@ninux.org>
2018-05-08 22:29:10 +0200

    [connections] Updated runtests.py to use new setting file

--- runtests.py
+++ runtests.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 sys.path.insert(0, "tests")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openwisp2.settings")
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
