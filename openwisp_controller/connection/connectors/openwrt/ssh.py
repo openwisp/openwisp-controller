00493159808e52423c4851ba6ac4004b2340507d
Federico Capoano <nemesis@ninux.org>
2018-05-10 21:32:20 +0200

    [connection] Added exec_command method

--- openwisp_controller/connection/connectors/openwrt/ssh.py
+++ openwisp_controller/connection/connectors/openwrt/ssh.py
@@ -3,4 +3,4 @@ from ..ssh import Ssh
 
 class OpenWrt(Ssh):
     def update_config(self):
-        self.shell.exec_command('/etc/init.d/openwisp_config restart')
+        self.exec_command('/etc/init.d/openwisp_config restart')
