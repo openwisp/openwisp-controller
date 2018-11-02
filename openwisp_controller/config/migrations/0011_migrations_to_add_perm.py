ee57df6e17605d1296235d4b92cecbceead18ceb
shashwat1002 <33834636+shashwat1002@users.noreply.github.com>
2018-11-02 18:43:05 +0530

    Revert "solve problems"
    This reverts commit 5485a64568d567d9567dead014109139b5fff8e7, reversing
    changes made to 3db7be9fa301db287bcfffa95a91c408be862c3d.

--- openwisp_controller/config/migrations/0011_migrations_to_add_perm.py
+++ openwisp_controller/config/migrations/0011_migrations_to_add_perm.py
@@ -1,9 +1,5 @@
 from django.db import migrations 
-<<<<<<< HEAD
 from django.models import Permission 
-=======
-
->>>>>>> f849d471375576117a2d4948ca7ba9c322818a4d
 
 
 def assignPerm(apps, schema_editor): 
@@ -19,17 +15,14 @@ def assignPerm(apps, schema_editor):
             admin.permissions.add(Permission.objects.get(codename="{}_{}".format(j,i)), bulk=True)
             operator.permissions.add(Permission.objects.get(codename="{}_{}".format(j,i)), bulk=True)
     for i in operators_read_only_admins_manage:
-<<<<<<< HEAD
         try:
             permission=Permission.objects.get(codename="view_{}".format(i))
             operator.permissions.add(permission, bulk=True)
         except Permission.DoesNotExist:
             pass   
         
-=======
-        operator.permissions.add(Permission.objects.get(codename="view_{}".format(i)), bulk=True)
->>>>>>> f849d471375576117a2d4948ca7ba9c322818a4d
-
+        for j in manage_operations: 
+            admin.permissions.add(Permissions.objects.get(codename="{}_{}".format(j,i)), bulk=True)
     
 
 class PermMigration(migrations.Migration): 
