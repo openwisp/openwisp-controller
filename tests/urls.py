70c85ee87715770fb727576d0006181db8039e01
Federico Capoano <nemesis@ninux.org>
2018-12-02 17:45:47 +0100

    [tests] Minor changes to debug-toolbar urls in test env

--- tests/urls.py
+++ tests/urls.py
@@ -22,4 +22,6 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
     import debug_toolbar
-    urlpatterns.append(url(r'^__debug__/', include(debug_toolbar.urls)))
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    ]
