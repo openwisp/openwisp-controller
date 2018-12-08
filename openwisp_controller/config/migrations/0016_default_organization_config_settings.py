d00b731d5fa06376837ccf823f602d3ea75b73b8
Federico Capoano <nemesis@ninux.org>
2018-12-08 18:20:26 +0100

    [migrations] Minor improvements to patch #73

--- openwisp_controller/config/migrations/0016_default_organization_config_settings.py
+++ openwisp_controller/config/migrations/0016_default_organization_config_settings.py
@@ -1,7 +1,7 @@
 from django.db import migrations
 
 
-def make_default_config_settings_organization(apps, schema_editor):
+def create_default_config_settings_organization(apps, schema_editor):
     organization_model = apps.get_model('openwisp_users', 'Organization')
     config_settings_model = apps.get_model(
         'config',
@@ -9,11 +9,11 @@ def make_default_config_settings_organization(apps, schema_editor):
     )
     for organization in organization_model.objects.all():
         try:
-            config_setting = organization.config_settings
+            organization.config_settings
         except organization_model.config_settings.RelatedObjectDoesNotExist:
-            # If there is no OrganizationConfigSettings associated with said organization
-            config_setting = config_settings_model.objects.create(organization=organization)
-            # creates OrganizationConfigSettings object corresponding to the organization
+            # if there is no OrganizationConfigSettings
+            # associated to this organization, create it
+            config_settings_model.objects.create(organization=organization)
 
 
 class Migration(migrations.Migration):
@@ -21,10 +21,9 @@ class Migration(migrations.Migration):
        ('openwisp_users', '0003_default_organization'),
        ('config', '0015_default_groups_permissions'),
     ]
-
     operations = [
         migrations.RunPython(
-            make_default_config_settings_organization,
+            create_default_config_settings_organization,
             reverse_code=migrations.RunPython.noop
         )
     ]
