487f13fb951f499a4c16144b47c456cbce93390e
Federico Capoano <nemesis@ninux.org>
2019-03-30 20:46:47 -0400

    [qa] Minor improvement of last commit

--- openwisp_controller/config/admin.py
+++ openwisp_controller/config/admin.py
@@ -22,7 +22,7 @@ class ConfigForm(AlwaysHasChangedMixin, AbstractConfigForm):
 
     def get_temp_model_instance(self, **options):
         config_model = self.Meta.model
-        instance = self.Meta.model(**options)
+        instance = config_model(**options)
         device_model = config_model.device.field.related_model
         org = Organization.objects.get(pk=self.data['organization'])
         instance.device = device_model(
