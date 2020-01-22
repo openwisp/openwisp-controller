9e1164322ed7db2be0ea7dcd87d7a6ed360c88df
Federico Capoano <federico.capoano@gmail.com>
2020-01-22 19:23:51 +0100

    [qa] Added missing data migration from django-netjsonconfig

---
+++ openwisp_controller/config/migrations/0022_vpn_format_dh.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+def format_dh(apps, schema_editor):
+    Vpn = apps.get_model('config', 'Vpn')
+
+    for vpn in Vpn.objects.all():
+        if vpn.dh.startswith("b'") and vpn.dh.endswith("'"):
+            vpn.dh = vpn.dh[2:-1]
+            vpn.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('config', '0021_vpn_key'),
+    ]
+
+    operations = [
+        migrations.RunPython(format_dh, reverse_code=migrations.RunPython.noop),
+    ]
