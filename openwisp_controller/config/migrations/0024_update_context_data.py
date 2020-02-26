d7f91bebd1a8cbbd43c0bb90075b76c8c4bf4fe8
Federico Capoano <federico.capoano@gmail.com>
2020-02-26 15:12:10 -0500

    [models] Updated context field

---
+++ openwisp_controller/config/migrations/0024_update_context_data.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+def forward(apps, schema_editor):
+    """
+    Updates default value of context field
+    """
+    if not schema_editor.connection.alias == 'default':
+        return
+    Config = apps.get_model('config', 'Config')
+
+    for config in Config.objects.filter(context__isnull=True):
+        config.context = {}
+        config.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('config', '0023_update_context'),
+    ]
+
+    operations = [
+        migrations.RunPython(forward, reverse_code=migrations.RunPython.noop),
+    ]
