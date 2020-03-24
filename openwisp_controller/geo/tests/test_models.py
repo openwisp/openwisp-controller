78a49f677d19c154ca443805bc513346366d6c5d
Noumbissi valere Gille Geovan <noumbissivalere@gmail.com>
2020-03-24 19:48:04 +0100

    [geo] Restrict to points only #151
    Fixed the issue by adding a clean method
    to the location model. This method
    ensures that when the geometry is not
    None, it should be an instance of a Point
    Geometry else a ValidationError is raised
    
    Fixes #151

--- openwisp_controller/geo/tests/test_models.py
+++ openwisp_controller/geo/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import LineString, Point, Polygon
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django_loci.tests.base.test_models import BaseTestModels
@@ -23,3 +24,21 @@ class TestModels(TestGeoMixin, BaseTestModels, TestCase):
             self.assertIn('location', e.message_dict)
         else:
             self.fail('ValidationError not raised')
+
+    def test_add_location_with_point_geometry(self):
+        self._create_location(geometry=Point(0, 0, srid=4326), name='point')
+        obj = self.location_model.objects.get(name='point')
+        self.assertEqual(obj.name, 'point')
+
+    def test_add_location_with_line_geometry(self):
+        with self.assertRaisesMessage(ValidationError, 'Only point geometry is allowed'):
+            self._create_location(geometry=LineString((0, 0), (1, 1), srid=4326), name='line')
+        obj = self.location_model.objects.filter(name='line')
+        self.assertEqual(obj.count(), 0)
+
+    def tes_add_location_with_polygon_geometry(self):
+        with self.assertRaisesMessage(ValidationError, 'Only point geometry is allowed'):
+            poly = Polygon((0, 0), (0, 1), (1, 1), (1, 0), (0, 0), srid=4326)
+            self._create_location(geometry=poly, name='poly')
+        obj = self.location_model.objects.filter(name='poly')
+        self.assertEqual(obj.count(), 0)
