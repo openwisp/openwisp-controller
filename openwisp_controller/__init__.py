7aa419ddc8d9cc7352014f5bec81a2cb08e198ff
Federico Capoano <federico.capoano@gmail.com>
2020-06-30 21:46:55 -0500

    [chores] Updated get_version to support post releases

--- openwisp_controller/__init__.py
+++ openwisp_controller/__init__.py
@@ -3,16 +3,18 @@ __version__ = VERSION  # alias
 
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
+    version = f'{VERSION[0]}.{VERSION[1]}'
     if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
+        version = f'{version}.{VERSION[2]}'
     if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
+        version = f'{version} pre-alpha'
+    if 'post' in VERSION[3]:
+        version = f'{version}.{VERSION[3]}'
     else:
         if VERSION[3] != 'final':
             try:
                 rev = VERSION[4]
             except IndexError:
                 rev = 0
-            version = '%s%s%s' % (version, VERSION[3][0:1], rev)
+            version = f'{version}{VERSION[3][0:1]}{rev}'
     return version
