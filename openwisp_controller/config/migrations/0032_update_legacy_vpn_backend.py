ac7c7f426fd75faf91144feb40958af7c864e7a8
Ajay Tripathi <ajay39in@gmail.com>
2020-07-11 04:18:06 +0530

    [fix] add missing legacy vpn backend migration #234
    * [fix] add missing legacy vpn backend migration #234
    
    Closes #234

---
+++ openwisp_controller/config/migrations/0032_update_legacy_vpn_backend.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+def update_legacy_vpn_backend(apps, schema_editor):
+    Vpn = apps.get_model('config', 'Vpn')
+    Vpn.objects.filter(backend='django_netjsonconfig.vpn_backends.OpenVpn').update(
+        backend='openwisp_controller.vpn_backends.OpenVpn'
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('config', '0031_update_vpn_dh_param'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            update_legacy_vpn_backend, reverse_code=migrations.RunPython.noop
+        ),
+    ]
