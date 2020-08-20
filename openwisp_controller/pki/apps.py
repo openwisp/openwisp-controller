830f1ca73aa38e5a724ec518bf3b9887ad4cbc18
Federico Capoano <federico.capoano@gmail.com>
2020-08-19 20:03:54 -0500

    [change] Avoid need of DJANGO_X509_CA_MODEL, DJANGO_X509_CERT_MODEL
    Let's keep things simple for our users.
    The pki models are the default ones, there's no need to require users to set the setting explicitly.
    
    This also simplifies things in ansible-openwisp2, docker-openwisp,
    openwisp-monitoring and openwisp-firmware-upgrader, since we won't
    have to update the settings of those projects, things will just keep working normally.
    
    I think this is the approach to follow whenever a new setting is added,
    we should provide a default value and avoid having the user to define it
    explicitly.

--- openwisp_controller/pki/apps.py
+++ openwisp_controller/pki/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+
+if not hasattr(settings, 'DJANGO_X509_CA_MODEL'):
+    setattr(settings, 'DJANGO_X509_CA_MODEL', 'pki.Ca')
+if not hasattr(settings, 'DJANGO_X509_CERT_MODEL'):
+    setattr(settings, 'DJANGO_X509_CERT_MODEL', 'pki.Cert')
 
 
 class PkiConfig(AppConfig):
