8e47cd966b1f830bff2d856c407355dddd252418
Federico Capoano <federico.capoano@gmail.com>
2020-09-23 17:29:33 -0500

    [chores] Base PkiConfig on DjangoX509Config

--- openwisp_controller/pki/apps.py
+++ openwisp_controller/pki/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django_x509.apps import DjangoX509Config
 
 if not hasattr(settings, 'DJANGO_X509_CA_MODEL'):
     setattr(settings, 'DJANGO_X509_CA_MODEL', 'pki.Ca')
@@ -8,6 +8,6 @@ if not hasattr(settings, 'DJANGO_X509_CERT_MODEL'):
     setattr(settings, 'DJANGO_X509_CERT_MODEL', 'pki.Cert')
 
 
-class PkiConfig(AppConfig):
+class PkiConfig(DjangoX509Config):
     name = 'openwisp_controller.pki'
     verbose_name = _('Public Key Infrastructure')
