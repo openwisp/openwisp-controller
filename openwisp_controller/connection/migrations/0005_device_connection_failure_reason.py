85e8cbc498822781ae5a4b506b5a04347297eab4
Gagan Deep <pandafy.dev@gmail.com>
2020-10-09 00:53:21 +0530

    [change] Changed "DeviceConnection.failure_reason" field to TextField #298
    Changed "openwisp_controller.connection.base.models.DeviceConnection" to
    "TextField" from "CharField" with limited size.
    
    This avoids possible exception if failed_reason is very long, which may happen in some corner cases.
    
    Fixes #298

---
+++ openwisp_controller/connection/migrations/0005_device_connection_failure_reason.py
@@ -0,0 +1,25 @@
+from django.db import migrations, models
+
+
+def truncate_failure_reason(apps, schema_editor):
+    DeviceConnection = apps.get_model('connection', 'DeviceConnection')
+
+    for device_connection in DeviceConnection.objects.iterator():
+        device_connection.failure_reason = device_connection.failure_reason[:128]
+        device_connection.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('connection', '0004_django3_1_upgrade'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='deviceconnection',
+            name='failure_reason',
+            field=models.TextField(blank=True, verbose_name='reason of failure'),
+        ),
+        migrations.RunPython(migrations.RunPython.noop, truncate_failure_reason,),
+    ]
