012770223b18846986f3b8c72fc31a53a4ccfb09
Gagan Deep <pandafy.dev@gmail.com>
2020-10-18 23:28:15 +0530

    [chores] Updated openwisp_notifications to 0.2 #308
    - Removed openwisp_notifications from `install-dev.sh`
      (openwisp_notification==0.2.1 is already released)
    - Removed openwsip_users from `install-dev.sh`
      (openwisp_users==0.4.1 is already released)
    - Added `rest` dependencies to openwisp_utils
    Closes #308
    Closes #282

--- install-dev.sh
+++ install-dev.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 set -e
-
-# TODO: remove when openwisp-notifications 0.2 is released
-pip install -U https://github.com/openwisp/openwisp-notifications/tarball/dev
-
-# TODO: can be removed when openwisp-users 0.4.1 is released
-# Commit needed for failures in tests:
-# https://github.com/openwisp/openwisp-users/commit/ef347927136ddb4676479cb54cdc7cd08049e2e5
-pip install -U --no-deps https://github.com/openwisp/openwisp-users/tarball/master
