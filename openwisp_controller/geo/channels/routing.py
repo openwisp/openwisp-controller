6844990676f0e99bf0140cee59715543c0f9e221
Gagan Deep <pandafy.dev@gmail.com>
2020-11-10 02:29:09 +0530

    [chores] Refactor URLs and routes for openwisp_notifications
    Bug:
      Interactive notifications were not working in the development
      environment.
    
    Remediation:
      - Fixed and refactored routes for websocket
      - Refactored URLs
      - Made test use "InMemoryChannelLayer", instead of "RedisChannelLayer"
    
    Co-authored-by: Gagan Deep <pandafy.dev@gmail.com>
    Co-authored-by: Federico Capoano <federico.capoano@gmail.com>

--- openwisp_controller/geo/channels/routing.py
+++ openwisp_controller/geo/channels/routing.py
@@ -3,16 +3,24 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.conf.urls import url
 from django_loci.channels.base import location_broadcast_path
-from openwisp_notifications.websockets.routing import get_routes
+from openwisp_notifications.websockets.routing import (
+    get_routes as get_notification_routes,
+)
 
 from .consumers import LocationBroadcast
 
-geo_routes = [url(location_broadcast_path, LocationBroadcast, name='LocationChannel')]
+
+def get_routes():
+    return [url(location_broadcast_path, LocationBroadcast, name='LocationChannel')]
+
+
+# Kept for backward compatibility
+geo_routes = get_routes()
 
 channel_routing = ProtocolTypeRouter(
     {
         'websocket': AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(get_routes() + geo_routes)),
+            AuthMiddlewareStack(URLRouter(get_notification_routes() + geo_routes)),
         )
     }
 )
