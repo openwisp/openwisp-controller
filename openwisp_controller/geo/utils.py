0d600bc8c0071585e87b190be6b522900ff4db6c
sanditya2510 <sanditya2510@gmail.com>
2020-12-31 08:24:46 +0530

    [fix] Removed deprecated location api url #350
    Removed the deprecated location api url from geo/utils.
    
    Fixes #350

--- openwisp_controller/geo/utils.py
+++ openwisp_controller/geo/utils.py
@@ -8,9 +8,4 @@ def get_geo_urls(geo_views):
             geo_views.device_location,
             name='api_device_location',
         ),
-        url(
-            r'^api/device-location/(?P<pk>[^/]+)/$',
-            geo_views.device_location,
-            name='api_device_location_deprecated',
-        ),
     ]
