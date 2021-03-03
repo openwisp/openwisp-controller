af20f498c8c9973a3b585a69a1054f5be0e1a1a7
Federico Capoano <f.capoano@openwisp.io>
2021-03-03 12:26:24 -0500

    [chores] Optimized query in AbstractTemplate._update_related_config_status
    Using values_list will fetch only the ID field, making the query smaller.

--- openwisp_controller/config/base/template.py
+++ openwisp_controller/config/base/template.py
@@ -127,13 +127,13 @@ class AbstractTemplate(ShareableOrgMixinUniqueName, BaseConfig):
             )
 
     def _update_related_config_status(self):
-        changing_status = list(self.config_relations.exclude(status='modified'))
+        changing_status = list(self.config_relations.exclude(status='modified').values_list('pk', flat=True))
         self.config_relations.update(status='modified')
         for config in self.config_relations.select_related('device').iterator():
             # config modified signal sent regardless
             config._send_config_modified_signal()
             # config status changed signal sent only if status changed
-            if config in changing_status:
+            if config.pk in changing_status:
                 config._send_config_status_changed_signal()
 
     def clean(self, *args, **kwargs):
