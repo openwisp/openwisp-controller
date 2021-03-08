2b73c32c3de43aee9b883af5ced1b098d4b8f94a
Gagan Deep <pandafy.dev@gmail.com>
2021-03-08 08:49:06 +0530

    [feature] Added automatic provisioning of Subnets and IPs #400
    - Added subnet division rules for defining charecterstic of
      provisioned subnets and IPs
    - Added "VPN" rule type that provisions subents and IPs for a
      device when a new VpnClient is created
    - Added support for adding custom subnet division rule types
    - Added support for updating subnet divisioon rules
      Currently only "number_of__ips" and "label" fields can be updated
    - Added an inline admin for subnet division rules in SubnetAdmin
    - Added a filter on SubnetAdmin to allow filtering by related device name
    - Added a filter on DeviceAdmin to allow filtering by related subnet
      It also allows filtering using parent of master subnet
    - Added setting for hiding provisioned subnets
    - Extends SubnetAdmin and IpAdmin from openwisp-ipam
    
    NOTE: Adds openwisp-ipam as direct dependency
    
    Closes #400

---
+++ openwisp_controller/subnet_division/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'openwisp_controller.subnet_division.apps.SubnetDivisionConfig'
---
+++ openwisp_controller/subnet_division/rule_types/vpn.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_delete, post_save
+
+from .base import BaseSubnetDivisionRuleType
+
+
+class VpnSubnetDivisionRuleType(BaseSubnetDivisionRuleType):
+    provision_signal = post_save
+    provision_sender = ('config', 'VpnClient')
+    provision_dispatch_uid = 'vpn_client_provision_subnet'
+
+    destroyer_signal = post_delete
+    destroyer_sender = provision_sender
+    destroyer_dispatch_uid = 'vpn_client_destroy_subnet'
+
+    organization_id_path = 'config.device.organization_id'
+    subnet_path = 'vpn.subnet'
