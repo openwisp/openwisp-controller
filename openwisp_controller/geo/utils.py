1621a8e698e9a43675aa26ac2e08f44f31dbfb3e
ManishShah120 <mkshah141@gmail.com>
2021-06-04 20:34:19 +0530

    [change!] Changed API endpoints of geo app #460
    Closes #460

--- openwisp_controller/geo/utils.py
+++ openwisp_controller/geo/utils.py
@@ -4,13 +4,17 @@ from django.urls import path
 def get_geo_urls(geo_views):
     return [
         path(
-            'api/v1/device/<str:pk>/location/',
+            'api/v1/controller/device/<str:pk>/location/',
             geo_views.device_location,
             name='device_location',
         ),
-        path('api/v1/location/geojson/', geo_views.geojson, name='location_geojson',),
         path(
-            'api/v1/location/<str:pk>/device/',
+            'api/v1/controller/location/geojson/',
+            geo_views.geojson,
+            name='location_geojson',
+        ),
+        path(
+            'api/v1/controller/location/<str:pk>/device/',
             geo_views.location_device_list,
             name='location_device_list',
         ),
