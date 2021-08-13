a9bd9946d82e3ac00b97a1b53b4462da392a2a2e
purhan <purhan01@gmail.com>
2021-08-13 19:46:33 +0530

    [chores] Requested changes
    - Minor changes in device.js
    - Revert changes in old migrations

--- openwisp_controller/config/static/config/js/device.js
+++ openwisp_controller/config/static/config/js/device.js
@@ -6,17 +6,17 @@ django.jQuery(function ($) {
 
   // replaces the management ip field with text with the option to edit it
   var ip_input = $(".field-management_ip > div > input");
-  var initial_ip = ip_input.prop("value");
+  var initial_ip = ip_input.val();
   ip_input.after(function () {
     ip_input.hide();
     return (
       '<span class="readonly" id="management_ip_text">' +
-      (initial_ip === "" ? "-n/a-" : initial_ip) +
+      (initial_ip === "" ? "-" : initial_ip) +
       "</span>"
     );
   });
   $("#management_ip_text").after(
-    '<a id="edit_management_ip">  <img src="/static/admin/img/icon-changelink.svg" alt="Edit"></a>'
+    '<a id="edit_management_ip"><img src="/static/admin/img/icon-changelink.svg" alt="Edit"></a>'
   );
   $("#edit_management_ip").click(function () {
     var ip_text = $("#management_ip_text");
