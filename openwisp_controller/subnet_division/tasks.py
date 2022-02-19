8e68108d0c1b7172390c2840902c114c5e537aaf
riteshh00 <riteshsinghh127@gmail.com>
2022-02-19 06:59:21 +0530

    [change] Added time limits to celery tasks #608
    Closes #608
    
    Co-authored-by: Gagan Deep <pandafy.dev@gmail.com>

--- openwisp_controller/subnet_division/tasks.py
+++ openwisp_controller/subnet_division/tasks.py
@@ -5,6 +5,8 @@ from django.db import transaction
 from django.utils.translation import gettext_lazy as _
 from swapper import load_model
 
+from openwisp_utils.tasks import OpenwispCeleryTask
+
 logger = logging.getLogger(__name__)
 
 Subnet = load_model('openwisp_ipam', 'Subnet')
@@ -69,7 +71,7 @@ def update_subnet_name_description(rule_id):
     )
 
 
-@shared_task
+@shared_task(base=OpenwispCeleryTask)
 def provision_extra_ips(rule_id, old_number_of_ips):
     def _create_ipaddress_and_subnetdivision_index_objects(ips, indexes):
         IpAddress.objects.bulk_create(ips)
