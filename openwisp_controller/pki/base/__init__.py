b0ad9979124bc2ef80a35d58b89665836250634c
Federico Capoano <f.capoano@openwisp.io>
2022-05-17 18:41:53 -0400

    [fix] Show reversion button to operators too
    For some obscure reasons the django-reversion button
    was not being shown to operators.
    
    I concluded this was not done on purpose and proceeded
    to remove the bogus code.

--- openwisp_controller/pki/base/__init__.py
+++
@@ -1,2 +0,0 @@
-class PkiReversionTemplatesMixin(object):
-    change_list_template = 'pki/change_list.html'
--- openwisp_controller/tests/utils.py
+++ openwisp_controller/tests/utils.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 from openwisp_users.tests.utils import TestMultitenantAdminMixin
 
@@ -6,6 +7,13 @@ user_model = get_user_model()
 
 
 class TestAdminMixin(TestMultitenantAdminMixin):
+    def _test_changelist_recover_deleted(self, app_label, model_label):
+        self._test_multitenant_admin(
+            url=reverse('admin:{0}_{1}_changelist'.format(app_label, model_label)),
+            visible=[],
+            hidden=[],
+        )
+
     def _login(self, username='admin', password='tester'):
         self.client.force_login(user_model.objects.get(username=username))
 
