6730872dbd9ad0331ae843f2bd194c97ba59f352
Aryaman <aryamanz29@gmail.com>
2023-01-18 20:07:30 +0530

    [chores] Used openwisp-users ProtectedAPIMixin

--- openwisp_controller/mixins.py
+++ openwisp_controller/mixins.py
@@ -1,14 +1,6 @@
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-from openwisp_users.api.authentication import BearerAuthentication
 from openwisp_users.api.mixins import FilterByOrganizationManaged
-from openwisp_users.api.permissions import DjangoModelPermissions
+from openwisp_users.api.mixins import ProtectedAPIMixin as BaseProtectedAPIMixin
 
 
-class ProtectedAPIMixin(FilterByOrganizationManaged):
-    authentication_classes = [BearerAuthentication, SessionAuthentication]
-    permission_classes = [
-        IsAuthenticated,
-        DjangoModelPermissions,
-    ]
+class ProtectedAPIMixin(BaseProtectedAPIMixin, FilterByOrganizationManaged):
+    pass
