f9c44df8eb2ceed15035caafbbe56848a536bbf6
Aryamanz29 <aryamanz29@gmail.com>
2023-08-04 02:36:49 +0530

    [fix] Fixed migration merge conflicts

--- openwisp_controller/config/migrations/0053_vpnclient_zt_identity_secret.py
+++ openwisp_controller/config/migrations/0053_vpnclient_zt_identity_secret.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('config', '0051_vpn_node_network_id'),
+        ('config', '0052_vpn_node_network_id'),
     ]
 
     operations = [
