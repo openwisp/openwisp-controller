bac95e7ccbf83612be93bf2236e7e727246cfe10
Gagan Deep <pandafy.dev@gmail.com>
2024-09-04 18:10:50 +0530

    [chores] Added compatibility with django-allauth~=0.63.6

--- tests/openwisp2/settings.py
+++ tests/openwisp2/settings.py
@@ -84,6 +84,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
