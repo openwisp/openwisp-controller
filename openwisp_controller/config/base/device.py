36783b8f6064484a25d2ddace58b6f3f8de3dbd9
Federico Capoano <federico.capoano@gmail.com>
2024-12-23 13:22:12 +0100

    [chores] Update openwisp_controller/config/base/device.py

--- openwisp_controller/config/base/device.py
+++ openwisp_controller/config/base/device.py
@@ -261,8 +261,8 @@ class AbstractDevice(OrgMixin, BaseModel):
                 )
 
     def clean(self, *args, **kwargs):
-        self.mac_address = self.mac_address.upper()
         super().clean(*args, **kwargs)
+        self.mac_address = self.mac_address.upper()
         self._validate_unique_name()
         self._validate_org_relation('group', field_error='group')
         self._validate_org_device_limit()
