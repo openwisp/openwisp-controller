7b24195b061d60fe8e1ee78c3c0ffb2197adce55
atlas <filippociandy@Filippos-Air.lan>
2025-03-06 17:52:25 -0700

    Changed file name and resolve qa check

--- openwisp_controller/config/migrations/0056_ow_zt_to_global.py
+++ openwisp_controller/config/migrations/0056_ow_zt_to_global.py
@@ -1,18 +1,22 @@
 from django.db import migrations
 import json
 
+
 def change_owzt_to_global(apps, schema_editor):
     Template = apps.get_model('config', 'Template')
-    for template in Template.objects.filter(type='vpn',vpn__backend='openwisp_controller.vpn_backends.ZeroTier'):
+    for template in Template.objects.filter(
+        type='vpn', vpn__backend='openwisp_controller.vpn_backends.ZeroTier'):
         config = json.loads(template.config)
         if 'zerotier' in config:
-            for item in config.get('zerotier',[]):
+            for item in config.get('zerotier', []):
                 if item.get('name') == 'ow_zt':
                     item['name'] = 'global'
+
 
 class Migration(migrations.Migration):
     dependencies = [('config', '0055_alter_config_status')]
 
     operations = [
-        migrations.RunPython(change_owzt_to_global, reverse_code=migrations.RunPython.noop)
+        migrations.RunPython(
+            change_owzt_to_global, reverse_code=migrations.RunPython.noop)
     ]
