456c201e8425569c49daa5d7326e77d5d705677e
Filippo Ciandy <filippo.ciandy10@gmail.com>
2025-03-10 19:40:04 -0600

    Update openwisp_controller/config/migrations/0056_ow_zt_to_global.py
    Fix issue so that it saves updated templates
    
    Co-authored-by: Gagan Deep <the.one.above.all.titan@gmail.com>

--- openwisp_controller/config/migrations/0056_ow_zt_to_global.py
+++ openwisp_controller/config/migrations/0056_ow_zt_to_global.py
@@ -1,16 +1,19 @@
 from django.db import migrations
-import json
+from django.db import migrations
 
 
 def change_owzt_to_global(apps, schema_editor):
     Template = apps.get_model('config', 'Template')
+    updated_templates = set()
     for template in Template.objects.filter(
-        type='vpn', vpn__backend='openwisp_controller.vpn_backends.ZeroTier'):
-        config = json.loads(template.config)
-        if 'zerotier' in config:
-            for item in config.get('zerotier', []):
+        type='vpn', vpn__backend='openwisp_controller.vpn_backends.ZeroTier'
+    ):
+        if 'zerotier' in template.config:
+            for item in template.config.get('zerotier', []):
                 if item.get('name') == 'ow_zt':
                     item['name'] = 'global'
+                    updated_templates.add(template)
+    Template.objects.bulk_update(updated_templates, ['config'])
 
 
 class Migration(migrations.Migration):
