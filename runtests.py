fa4d7e98e0d2b12fefb1654ac7088be606ad2595
Gagan Deep <pandafy.dev@gmail.com>
2025-03-18 01:15:27 +0530

    [fix] Fixed runtests.py

--- runtests.py
+++ runtests.py
@@ -2,40 +2,46 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 import sys
 
 import pytest
-from django.core.management import execute_from_command_line
 
 
 def run_tests(args, settings_module):
     """
-    Run Django tests with the specified settings module.
+    Run Django tests with the specified settings module in a separate subprocess.
+    Executes the command inside the 'tests/' directory.
     """
     os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
-    execute_from_command_line(args)
+    result = subprocess.run(['python', 'manage.py'] + args, cwd='tests')
+    if result.returncode != 0:
+        sys.exit(result.returncode)  # Exit immediately if tests fail
 
 
 if __name__ == '__main__':
-    sys.path.insert(0, 'tests')
-
-    args = sys.argv
-    args.insert(1, 'test')
+    base_args = sys.argv.copy()[1:]
     if not os.environ.get('SAMPLE_APP', False):
-        args.insert(2, 'openwisp_controller')
+        test_app = 'openwisp_controller'
         app_dir = 'openwisp_controller/'
     else:
-        args.insert(2, 'openwisp2')
+        test_app = 'openwisp2'
         app_dir = 'tests/openwisp2/'
-
     # Run all tests except Selenium tests using SQLite
-    sqlite_args = args.copy()
-    sqlite_args.extend(['--exclude-tag', 'selenium_tests'])
-    run_tests(sqlite_args, settings_module='openwisp2.settings')
+    sqlite_args = ['test', test_app, '--exclude-tag', 'selenium_tests'] + base_args
+    run_tests(sqlite_args, 'openwisp2.settings')
 
     # Run Selenium tests using PostgreSQL
-    psql_args = args.copy()
-    psql_args.extend(['--tag', 'selenium_tests', '--tag', 'db_tests'])
-    run_tests(psql_args, settings_module='openwisp2.postgresql_settings')
+    psql_args = [
+        'test',
+        test_app,
+        '--tag',
+        'db_tests',
+        '--tag',
+        'selenium_tests',
+    ] + base_args
+    run_tests(psql_args, 'openwisp2.postgresql_settings')
 
+    # Run pytest tests
+    sys.path.insert(0, 'tests')
     sys.exit(pytest.main([app_dir]))
