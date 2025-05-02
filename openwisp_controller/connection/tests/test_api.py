523f4dab1b84d4b0611085ec1fab60aeb65e7470
Federico Capoano <f.capoano@openwisp.io>
2025-05-02 13:13:28 -0300

    [change] Improved consistency of REST API
    * [change] Renamed serializer to serializers in connection.api (consistency)
    * [change] Centralize the base serializer logic of the REST API
    * [fix] Fixed bug with new basic config and different org templates
    * [chores] Increased consistency of connection API pk params
    * [chores] PKI set URL param type to int
    * [chores] Config API: use uuid path converter

--- openwisp_controller/connection/tests/test_api.py
+++ openwisp_controller/connection/tests/test_api.py
@@ -503,7 +503,7 @@ class TestConnectionApi(
             'enabled': False,
             'failure_reason': '',
         }
-        with self.assertNumQueries(15):
+        with self.assertNumQueries(14):
             response = self.client.put(path, data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
@@ -517,7 +517,7 @@ class TestConnectionApi(
         path = reverse('connection_api:deviceconnection_detail', args=(d1, dc.pk))
         self.assertEqual(dc.update_strategy, app_settings.UPDATE_STRATEGIES[0][0])
         data = {'update_strategy': app_settings.UPDATE_STRATEGIES[1][0]}
-        with self.assertNumQueries(14):
+        with self.assertNumQueries(13):
             response = self.client.patch(path, data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
