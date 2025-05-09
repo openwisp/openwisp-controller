3222398be0c74ec95709ec4a78ae682bfc375cc0
Gagan Deep <pandafy.dev@gmail.com>
2025-05-09 23:08:22 +0530

    [tests] Added tests for multi-tenant admin

--- openwisp_controller/connection/api/serializers.py
+++ openwisp_controller/connection/api/serializers.py
@@ -51,7 +51,6 @@ class CommandSerializer(ValidatedDeviceFieldSerializer):
 
 class CredentialSerializer(BaseSerializer):
     params = serializers.JSONField()
-    include_shared = True
 
     class Meta:
         model = Credentials
