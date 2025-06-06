40f7b91b872c62c032138f1316d9c269c43e929f
stktyagi <stktyagi2810@gmail.com>
2025-06-06 12:27:25 +0530

    [chores] Fixed and updated recency tests #1040
    Added and tested recency tests for config /group and /device endpoints. Updated tests(added as subtests) for previously fixed and recently fixed endpoints.
    
    Fixes #1040

--- openwisp_controller/connection/tests/test_api.py
+++ openwisp_controller/connection/tests/test_api.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 from django.contrib.auth.models import Permission
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.timezone import now, timedelta
 from packaging.version import parse as parse_version
 from rest_framework import VERSION as REST_FRAMEWORK_VERSION
 from rest_framework.exceptions import ErrorDetail
@@ -399,6 +400,16 @@ class TestConnectionApi(
         with self.assertNumQueries(4):
             response = self.client.get(path)
         self.assertEqual(response.status_code, 200)
+        with self.subTest("Check ordering of credentials"):
+            cred_old = self._create_credentials(name="Old Credential")
+            cred_old.created = now() - timedelta(days=1)
+            cred_old.save()
+            self._create_credentials(name="Newest Credential")
+            response = self.client.get(path)
+            self.assertEqual(response.status_code, 200)
+            created_list = [cred["created"] for cred in response.data["results"]]
+            sorted_created = sorted(created_list, reverse=True)
+            self.assertEqual(created_list, sorted_created)
 
     def test_filter_credentials_list(self):
         cred_1 = self._create_credentials(name="Credential One")
@@ -416,18 +427,6 @@ class TestConnectionApi(
         self.assertEqual(response.data["count"], 1)
         self.assertContains(response, cred_2.id)
         self.assertNotContains(response, cred_1.id)
-
-    @patch.object(ListViewPagination, "page_size", 3)
-    def test_credential_list_ordering(self):
-        number_of_credentials = 6
-        url = reverse("connection_api:credential_list")
-        for i in range(number_of_credentials):
-            self._create_credentials(name=f"Test Credential {i}")
-        response = self.client.get(url)
-        self.assertEqual(response.data["count"], number_of_credentials)
-        created_list = [cred["created"] for cred in response.data["results"]]
-        sorted_created_list = sorted(created_list, reverse=True)
-        self.assertEqual(created_list, sorted_created_list)
 
     def test_post_credential_list(self):
         path = reverse("connection_api:credential_list")
@@ -502,25 +501,19 @@ class TestConnectionApi(
             response = self.client.get(path)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["count"], 0)
-
-    @patch.object(ListViewPagination, "page_size", 3)
-    def test_deviceconnection_list_ordering(self):
-        device = self._create_device()
-        self._create_config(device=device)
-        credentials_list = [
-            self._create_credentials(name=f"Credential {i}") for i in range(3)
-        ]
-        for cred in credentials_list:
-            DeviceConnection.objects.create(
-                device=device,
-                credentials=cred,
-            )
-        path = reverse("connection_api:deviceconnection_list", args=(device.pk,))
-        response = self.client.get(path)
-        self.assertEqual(response.status_code, 200)
-        created_list = [item["created"] for item in response.data["results"]]
-        sorted_created_list = sorted(created_list, reverse=True)
-        self.assertEqual(created_list, sorted_created_list)
+        with self.subTest("Check ordering of device connections"):
+            self._create_config(device=d1)
+            creds = [self._create_credentials(name=f"Cred {i}") for i in range(3)]
+            creds[0].created = now() - timedelta(days=1)
+            creds[0].save()
+            for cred in creds:
+                DeviceConnection.objects.create(device=d1, credentials=cred)
+            response = self.client.get(path)
+            print(response.data)
+            self.assertEqual(response.status_code, 200)
+            created_list = [conn["created"] for conn in response.data["results"]]
+            sorted_created = sorted(created_list, reverse=True)
+            self.assertEqual(created_list, sorted_created)
 
     def test_post_deviceconnection_list(self):
         d1 = self._create_device()
