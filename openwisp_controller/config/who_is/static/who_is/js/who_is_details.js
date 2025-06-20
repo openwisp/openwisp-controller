0f77a30b80a886b4e78e16c42c20fb3a9681677b
DragnEmperor <dragnemperor@gmail.com>
2025-06-21 00:43:17 +0530

    [tests] Added selenium test case for UI
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

--- openwisp_controller/config/who_is/static/who_is/js/who_is_details.js
+++ openwisp_controller/config/who_is/static/who_is/js/who_is_details.js
@@ -6,13 +6,13 @@ django.jQuery(function ($) {
   var parentDiv = $("#overview-group .field-last_ip div:last"),
     whoIsInfoUrl =
       window.location.origin + "/admin/config/device/get-who-is-info/";
-  whoIsInfoUrl += "?device_id=" + $('input[name="config-0-device"]').val();
+  whoIsInfoUrl += "?device_id=" + $('input[id="id_uuid"]').val();
 
   $.get(whoIsInfoUrl).done(function (data) {
     parentDiv.after(
       `<div>These details are based on last public IP: ${data.ip_address}</div>` +
         `<table id="who_is_table"><tr><th>ISP <span title="This is the Organization associated with registered ASN" id="isp_info">i</span></th><th>Country</th></tr><tr><td>${data.isp}</td><td>${data.address.country}</td></tr></table>` +
-        `<details id="who_is_details"><summary><span class="who_is_globe"></span><div><span>Additional Details</span><span class="mg-arrow"></span></div></summary><div><p>ASN : ${data.asn}</p><p>Timezone : ${data.timezone}</p><p>Address : ${data.address}</p><p>CIDR : ${data.cidr}</p></div></details>`,
+        `<details id="who_is_details"><summary><span class="who_is_globe"></span><div><span>Additional Details</span><span class="mg-arrow"></span></div></summary><div><p>ASN : ${data.asn}</p><p>Timezone : ${data.timezone}</p><p>Address : ${data.formatted_address}</p><p>CIDR : ${data.cidr}</p></div></details>`,
     );
   });
 });
