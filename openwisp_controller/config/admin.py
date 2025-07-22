7d83db3ec8b1d2895fed2102ea6e6c2b7d1d0117
JgtAman <41839176+DragnEmperor@users.noreply.github.com>
2025-07-22 23:25:24 +0530

    [feature] Added WHOIS details to the UI #1026
    Closes #1026
    
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

--- openwisp_controller/config/admin.py
+++ openwisp_controller/config/admin.py
@@ -31,6 +31,7 @@ from openwisp_ipam.filters import SubnetFilter
 from swapper import load_model
 
 from openwisp_controller.config.views import get_default_values, get_relevant_templates
+from openwisp_controller.config.whois.utils import get_whois_info
 from openwisp_users.admin import OrganizationAdmin
 from openwisp_users.multitenancy import MultitenantOrgFilter
 from openwisp_utils.admin import (
@@ -49,6 +50,7 @@ from .widgets import DeviceGroupJsonSchemaWidget, JsonSchemaWidget
 
 logger = logging.getLogger(__name__)
 prefix = "config/"
+whois_prefix = "whois/"
 Config = load_model("config", "Config")
 Device = load_model("config", "Device")
 DeviceGroup = load_model("config", "DeviceGroup")
@@ -569,12 +571,21 @@ class DeviceAdmin(MultitenantAdminMixin, BaseConfigAdmin, UUIDAdmin):
         fields.insert(0, "hardware_id")
     list_select_related = ("config", "organization")
 
-    class Media(BaseConfigAdmin.Media):
+    # overriding media property to allow testing in isolation
+    # as class Media is evaluated at import time making the
+    # settings not overridable in tests
+    @property
+    def media(self):
         js = BaseConfigAdmin.Media.js + [
             f"{prefix}js/tabs.js",
             f"{prefix}js/management_ip.js",
             f"{prefix}js/relevant_templates.js",
         ]
+        css = BaseConfigAdmin.Media.css["all"]
+        if app_settings.WHOIS_CONFIGURED:
+            js.append(f"{whois_prefix}js/whois.js")
+            css += (f"{whois_prefix}css/whois.css",)
+        return super().media + forms.Media(js=js, css={"all": css})
 
     def has_change_permission(self, request, obj=None):
         perm = super().has_change_permission(request)
@@ -928,6 +939,10 @@ class DeviceAdmin(MultitenantAdminMixin, BaseConfigAdmin, UUIDAdmin):
                 ),
             }
         )
+        # passing the whois details to the context to avoid
+        # the need to make an additional request in the js
+        if data := get_whois_info(pk):
+            ctx["device_whois_details"] = json.dumps(data)
         return ctx
 
     def add_view(self, request, form_url="", extra_context=None):
