58dbbfce6d7f2c2d0e494e6f1ba0791c80cbdb2e
DragnEmperor <dragnemperor@gmail.com>
2025-07-23 23:22:46 +0530

    [enhancement] Notification in task, alerts on admin
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

--- openwisp_controller/config/whois/utils.py
+++ openwisp_controller/config/whois/utils.py
@@ -22,16 +22,36 @@ MESSAGE_MAP = {
         "description": _("Multiple devices found for IP: {ip_address}"),
         "level": "error",
     },
+    "location_created": {
+        "message": _(
+            "Approximate location [{notification.actor}]({notification.actor_link})"
+            " for device"
+            " [{notification.target}]({notification.target_link}#devicelocation-group)"
+            " created successfully."
+        ),
+        "description": _("Location created for IP: {ip_address}"),
+        "level": "info",
+    },
+    "location_updated": {
+        "message": _(
+            "Approximate location [{notification.actor}]({notification.actor_link})"
+            " for device"
+            " [{notification.target}]({notification.target_link}#devicelocation-group)"
+            " updated successfully."
+        ),
+        "description": _("Location updated for IP: {ip_address}"),
+        "level": "info",
+    },
 }
 
 
-def send_whois_task_notification(device_pk, notify_type):
+def send_whois_task_notification(device_pk, notify_type, actor=None):
     Device = load_model("config", "Device")
 
     device = Device.objects.get(pk=device_pk)
     notify_details = MESSAGE_MAP[notify_type]
     notify.send(
-        sender=device,
+        sender=actor or device,
         type="generic_message",
         target=device,
         action_object=device,
--- openwisp_controller/geo/admin.py
+++ openwisp_controller/geo/admin.py
@@ -1,5 +1,5 @@
 import reversion
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils.translation import gettext_lazy as _
 from django_loci.base.admin import (
     AbstractFloorPlanAdmin,
@@ -99,6 +99,16 @@ class LocationAdmin(MultitenantAdminMixin, AbstractLocationAdmin):
     inlines = [FloorPlanInline]
     list_select_related = ("organization",)
     readonly_fields = ("is_estimated",)
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        obj = self.get_object(request, object_id)
+        if obj and obj.is_estimated:
+            messages.warning(
+                request,
+                "This location is estimated based on the device's last IP address."
+                " Please refine it for greater accuracy.",
+            )
+        return super().change_view(request, object_id, form_url, extra_context)
 
 
 LocationAdmin.list_display.insert(1, "organization")
