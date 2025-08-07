1a63575b9a45963ac05b90bc4b51d400ac44cc21
DragnEmperor <dragnemperor@gmail.com>
2025-08-08 01:17:41 +0530

    [enhancement] Added filters for admin, api
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

---
+++ openwisp_controller/geo/estimated_location/mixins.py
@@ -0,0 +1,30 @@
+from .utils import check_estimate_location_configured
+
+
+class EstimatedLocationMixin:
+    """
+    Serializer mixin to add estimated location field to the serialized data
+    if the estimated location feature is configured and enabled for the organization.
+    """
+
+    def to_representation(self, obj):
+        data = super().to_representation(obj)
+        if check_estimate_location_configured(obj):
+            data["is_estimated"] = obj.is_estimated
+        else:
+            data.pop("is_estimated", None)
+        return data
+
+
+class EstimatedLocationGeoJsonSerializer(EstimatedLocationMixin):
+    """
+    Extension of EstimatedLocationMixin for GeoJSON serialization.
+    """
+
+    def to_representation(self, obj):
+        data = super(EstimatedLocationMixin, self).to_representation(obj)
+        if check_estimate_location_configured(obj):
+            data["properties"]["is_estimated"] = obj.is_estimated
+        else:
+            data["properties"].pop("is_estimated", None)
+        return data
