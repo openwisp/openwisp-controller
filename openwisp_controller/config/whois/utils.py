8aca0dcc9ded47fbca3458008579493cab8dfdb1
JgtAman <41839176+DragnEmperor@users.noreply.github.com>
2025-08-21 01:53:42 +0530

    [feature] Estimated location: added notification and admin warning #1035
    Closes #1035
    
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

--- openwisp_controller/config/whois/utils.py
+++ openwisp_controller/config/whois/utils.py
@@ -1,43 +1,57 @@
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from openwisp_notifications.signals import notify
 from swapper import load_model
 
 from .. import settings as app_settings
 
 MESSAGE_MAP = {
-    "device_error": {
+    "whois_device_error": {
+        "type": "generic_message",
+        "level": "error",
         "message": _(
             "Failed to fetch WHOIS details for device"
             " [{notification.target}]({notification.target_link})"
         ),
         "description": _("WHOIS details could not be fetched for ip: {ip_address}."),
-        "level": "error",
     },
-    "location_error": {
+    "estimated_location_error": {
+        "level": "error",
+        "type": "estimated_location_info",
         "message": _(
             "Unable to create estimated location for device "
             "[{notification.target}]({notification.target_link}). "
             "Please assign/create a location manually."
         ),
         "description": _("Multiple devices found for IP: {ip_address}"),
-        "level": "error",
+    },
+    "estimated_location_created": {
+        "type": "estimated_location_info",
+        "description": _("Estimated Location {notification.verb} for IP: {ip_address}"),
+    },
+    "estimated_location_updated": {
+        "type": "estimated_location_info",
+        "message": _(
+            "Estimated location [{notification.actor}]({notification.actor_link})"
+            " for device"
+            " [{notification.target}]({notification.target_link})"
+            " updated successfully."
+        ),
+        "description": _("Estimated Location updated for IP: {ip_address}"),
     },
 }
 
 
-def send_whois_task_notification(device_pk, notify_type):
+def send_whois_task_notification(device_pk, notify_type, actor=None):
     Device = load_model("config", "Device")
 
     device = Device.objects.get(pk=device_pk)
     notify_details = MESSAGE_MAP[notify_type]
     notify.send(
-        sender=device,
-        type="generic_message",
+        sender=actor or device,
         target=device,
         action_object=device,
-        level=notify_details["level"],
-        message=notify_details["message"],
-        description=notify_details["description"].format(ip_address=device.last_ip),
+        ip_address=device.last_ip,
+        **notify_details,
     )
 
 
