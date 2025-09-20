dbb78859da3c28c639be2ef04798c05cee402e05
JgtAman <41839176+DragnEmperor@users.noreply.github.com>
2025-09-20 21:51:12 +0530

    [feature] Allow estimating geographic location based on WHOIS data #1034
    - Estimated geographic location logic, closes #1034.
    - Added notification and admin warning, closes #1035.
    - Set location estimated flag to ``False`` on manual update,
      closes #1036.
    - Added admin and API filters for estimated locations,
      closes #1028.
    
    ---------
    
    Signed-off-by: DragnEmperor <dragnemperor@gmail.com>

---
+++ openwisp_controller/config/whois/tests/utils.py
@@ -0,0 +1,152 @@
+from unittest import mock
+
+from django.contrib.gis.geos import Point
+from django.urls import reverse
+from swapper import load_model
+
+from ...tests.utils import CreateConfigMixin
+
+Device = load_model("config", "Device")
+WHOISInfo = load_model("config", "WHOISInfo")
+OrganizationConfigSettings = load_model("config", "OrganizationConfigSettings")
+
+
+class CreateWHOISMixin(CreateConfigMixin):
+    def _create_whois_info(self, **kwargs):
+        options = dict(
+            ip_address="172.217.22.14",
+            address={
+                "city": "Mountain View",
+                "country": "United States",
+                "continent": "North America",
+                "postal": "94043",
+            },
+            asn="15169",
+            isp="Google LLC",
+            timezone="America/Los_Angeles",
+            cidr="172.217.22.0/24",
+            coordinates=Point(150, 50, srid=4326),
+        )
+
+        options.update(kwargs)
+        w = WHOISInfo(**options)
+        w.full_clean()
+        w.save()
+        return w
+
+    def setUp(self):
+        super().setUp()
+        OrganizationConfigSettings.objects.create(
+            organization=self._get_org(), whois_enabled=True
+        )
+
+
+class WHOISTransactionMixin:
+    @staticmethod
+    def _mocked_client_response():
+        mock_response = mock.MagicMock()
+        mock_response.city.name = "Mountain View"
+        mock_response.country.name = "United States"
+        mock_response.continent.name = "North America"
+        mock_response.postal.code = "94043"
+        mock_response.traits.autonomous_system_organization = "Google LLC"
+        mock_response.traits.autonomous_system_number = 15169
+        mock_response.traits.network = "172.217.22.0/24"
+        mock_response.location.time_zone = "America/Los_Angeles"
+        mock_response.location.latitude = 50
+        mock_response.location.longitude = 150
+        return mock_response
+
+    def _task_called(self, mocked_task, task_name="WHOIS lookup"):
+        org = self._get_org()
+
+        with self.subTest(f"{task_name} task called when last_ip is public"):
+            with mock.patch("django.core.cache.cache.set") as mocked_set:
+                device = self._create_device(last_ip="172.217.22.14")
+                mocked_task.assert_called()
+                mocked_set.assert_called_once()
+        mocked_task.reset_mock()
+
+        with self.subTest(
+            f"{task_name} task called when last_ip is changed and is public"
+        ):
+            with mock.patch("django.core.cache.cache.get") as mocked_get, mock.patch(
+                "django.core.cache.cache.set"
+            ) as mocked_set:
+                device.last_ip = "172.217.22.10"
+                device.save()
+                mocked_task.assert_called()
+                mocked_set.assert_not_called()
+                mocked_get.assert_called_once()
+        mocked_task.reset_mock()
+
+        with self.subTest(f"{task_name} task not called when last_ip not updated"):
+            device.name = "default.test.Device2"
+            device.save()
+            mocked_task.assert_not_called()
+        mocked_task.reset_mock()
+
+        with self.subTest(f"{task_name} task not called when last_ip is private"):
+            device.last_ip = "10.0.0.1"
+            device.save()
+            mocked_task.assert_not_called()
+        mocked_task.reset_mock()
+
+        with self.subTest(f"{task_name} task not called when WHOIS is disabled"):
+            Device.objects.all().delete()
+            org.config_settings.whois_enabled = False
+            # Invalidates old org config settings cache
+            org.config_settings.save(update_fields=["whois_enabled"])
+            device = self._create_device(last_ip="172.217.22.14")
+            mocked_task.assert_not_called()
+        mocked_task.reset_mock()
+
+        with self.subTest(
+            f"{task_name} task called via DeviceChecksumView when WHOIS is enabled"
+        ):
+            org.config_settings.whois_enabled = True
+            # Invalidates old org config settings cache
+            org.config_settings.save(update_fields=["whois_enabled"])
+            # config is required for checksum view to work
+            device.refresh_from_db()
+            self._create_config(device=device)
+            # setting remote address field to a public IP to trigger WHOIS task
+            # since the view uses this header for tracking the device's IP
+            response = self.client.get(
+                reverse("controller:device_checksum", args=[device.pk]),
+                {"key": device.key},
+                REMOTE_ADDR="172.217.22.10",
+            )
+            self.assertEqual(response.status_code, 200)
+            mocked_task.assert_called()
+        mocked_task.reset_mock()
+
+        with self.subTest(
+            f"{task_name} task called via DeviceChecksumView for no WHOIS record"
+        ):
+            WHOISInfo.objects.all().delete()
+            device.refresh_from_db()
+            response = self.client.get(
+                reverse("controller:device_checksum", args=[device.pk]),
+                {"key": device.key},
+                REMOTE_ADDR=device.last_ip,
+            )
+            self.assertEqual(response.status_code, 200)
+            mocked_task.assert_not_called()
+        mocked_task.reset_mock()
+
+        with self.subTest(
+            f"{task_name} task not called via DeviceChecksumView when WHOIS is disabled"
+        ):
+            WHOISInfo.objects.all().delete()
+            device.refresh_from_db()
+            org.config_settings.whois_enabled = False
+            org.config_settings.save(update_fields=["whois_enabled"])
+            response = self.client.get(
+                reverse("controller:device_checksum", args=[device.pk]),
+                {"key": device.key},
+                REMOTE_ADDR=device.last_ip,
+            )
+            self.assertEqual(response.status_code, 200)
+            mocked_task.assert_not_called()
+        mocked_task.reset_mock()
--- openwisp_controller/geo/base/models.py
+++ openwisp_controller/geo/base/models.py
@@ -1,4 +1,9 @@
+import re
+
 from django.contrib.gis.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy as _
 from django_loci.base.models import (
     AbstractFloorPlan,
     AbstractLocation,
@@ -6,12 +11,80 @@ from django_loci.base.models import (
 )
 from swapper import get_model_name
 
+from openwisp_controller.config.whois.service import WHOISService
 from openwisp_users.mixins import OrgMixin, ValidateOrgMixin
 
 
 class BaseLocation(OrgMixin, AbstractLocation):
+    _changed_checked_fields = ["is_estimated", "address", "geometry"]
+
+    is_estimated = models.BooleanField(
+        default=False,
+        help_text=_("Whether the location's coordinates are estimated."),
+    )
+
     class Meta(AbstractLocation.Meta):
         abstract = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._set_initial_values_for_changed_checked_fields()
+
+    def _set_initial_values_for_changed_checked_fields(self):
+        deferred_fields = self.get_deferred_fields()
+        for field in self._changed_checked_fields:
+            if field in deferred_fields:
+                setattr(self, f"_initial_{field}", models.DEFERRED)
+            else:
+                setattr(self, f"_initial_{field}", getattr(self, field))
+
+    def clean(self):
+        # Raise validation error if `is_estimated` is True but estimated feature is
+        # disabled.
+        if (
+            (self._state.adding or self._initial_is_estimated != self.is_estimated)
+            and self.is_estimated
+            and not WHOISService.check_estimate_location_configured(
+                self.organization_id
+            )
+        ):
+            raise ValidationError(
+                {
+                    "is_estimated": _(
+                        "Estimated Location feature required to be configured."
+                    )
+                }
+            )
+        return super().clean()
+
+    def save(self, *args, _set_estimated=False, **kwargs):
+        """
+        Save the location object with special handling for estimated locations.
+
+        Parameters:
+            _set_estimated: Boolean flag to indicate if this save is being performed
+            by the estimated location system. When False (default),
+            manual edits will clear the estimated status.
+            *args, **kwargs: Arguments passed to the parent save method.
+
+        Returns:
+            The result of the parent save method.
+        """
+        if WHOISService.check_estimate_location_configured(self.organization_id):
+            if not _set_estimated and (
+                self._initial_address != self.address
+                or self._initial_geometry != self.geometry
+            ):
+                self.is_estimated = False
+                estimated_string = gettext("Estimated Location")
+                if self.name and estimated_string in self.name:
+                    # remove string starting with "(Estimated Location"
+                    self.name = re.sub(
+                        rf"\s\({estimated_string}.*", "", self.name, flags=re.IGNORECASE
+                    )
+        else:
+            self.is_estimated = self._initial_is_estimated
+        return super().save(*args, **kwargs)
 
 
 class BaseFloorPlan(OrgMixin, AbstractFloorPlan):
