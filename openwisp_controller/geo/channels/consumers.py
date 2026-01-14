998d255f8d2fd4bb0fb1d98d8fc81c9bc5ecf891
dee077 <deepanshu.sahu27@gmail.com>
2026-01-14 14:17:16 +0530

    [fix] Disconnect logic

--- openwisp_controller/geo/channels/consumers.py
+++ openwisp_controller/geo/channels/consumers.py
@@ -29,10 +29,14 @@ class CommonLocationBroadcast(BaseCommonLocationBroadcast):
         """
         if user.is_superuser:
             super().join_groups(user)
+            self.group_name = [self.group_name]
             return
-
-        self.group_names = []
+        self.group_name = []
         for org in user.organizations_managed:
             group = f"loci.mobile-location.organization.{org}"
-            self.group_names.append(group)
+            self.group_name.append(group)
             async_to_sync(self.channel_layer.group_add)(group, self.channel_name)
+
+    def disconnect(self, close_code):
+        for group in getattr(self, "group_name", []):
+            async_to_sync(self.channel_layer.group_discard)(group, self.channel_name)
