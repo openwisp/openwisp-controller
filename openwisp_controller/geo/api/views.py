129e314f74e37d562faec092da2aeea83f3e4336
Federico Capoano <f.capoano@openwisp.io>
2026-02-13 13:23:18 -0300

    [chores] Simplified is_estimated API filter

--- openwisp_controller/geo/api/views.py
+++ openwisp_controller/geo/api/views.py
@@ -51,13 +51,8 @@ class LocationOrganizationFilter(OrganizationManagedFilter):
     class Meta(OrganizationManagedFilter.Meta):
         model = Location
         fields = OrganizationManagedFilter.Meta.fields + ["is_mobile", "type"]
-
-    def __init__(self, data=None, queryset=None, *, request=None, prefix=None):
-        super().__init__(data, queryset, request=request, prefix=prefix)
         if config_app_settings.WHOIS_CONFIGURED:
-            self.filters["is_estimated"] = filters.BooleanFilter(
-                field_name="is_estimated"
-            )
+            fields.append("is_estimated")
 
 
 class FloorPlanOrganizationFilter(OrganizationManagedFilter):
