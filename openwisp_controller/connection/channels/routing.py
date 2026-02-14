96955e3af988987d428fb4d7184485337bb79e9f
stktyagi <stktyagi2810@gmail.com>
2026-02-14 17:30:52 +0530

    [fix] Fix websocket route with path converters #682
    Prevents WebSocket connection attempts with malformed or garbage-suffixed URLs
    
    Fixes #682

--- openwisp_controller/connection/channels/routing.py
+++ openwisp_controller/connection/channels/routing.py
@@ -1,16 +1,12 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import consumers as ow_consumer
-
-uuid_regex = (
-    r"[0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{12}"
-)
 
 
 def get_routes(consumer=ow_consumer):
     return [
-        re_path(
-            rf"^ws/controller/device/(?P<pk>{uuid_regex})/command$",
+        path(
+            "ws/controller/device/<uuid:pk>/command",
             consumer.CommandConsumer.as_asgi(),
         )
     ]
