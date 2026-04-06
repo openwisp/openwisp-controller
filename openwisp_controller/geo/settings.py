fc5eff20566a4a69d11486258550101eca656492
Federico Capoano <f.capoano@openwisp.io>
2026-04-06 19:05:41 -0300

    [chores] Avoided nested if

--- openwisp_controller/geo/settings.py
+++ openwisp_controller/geo/settings.py
@@ -6,9 +6,8 @@ from ..settings import get_setting
 ESTIMATED_LOCATION_ENABLED = get_setting("ESTIMATED_LOCATION_ENABLED", False)
 
 # Validate that WHOIS is enabled if estimated location is enabled
-if ESTIMATED_LOCATION_ENABLED:
-    if not config_settings.WHOIS_ENABLED:
-        raise ImproperlyConfigured(
-            "OPENWISP_CONTROLLER_WHOIS_ENABLED must be set to True before "
-            "setting OPENWISP_CONTROLLER_ESTIMATED_LOCATION_ENABLED to True."
-        )
+if ESTIMATED_LOCATION_ENABLED and not config_settings.WHOIS_ENABLED:
+    raise ImproperlyConfigured(
+        "OPENWISP_CONTROLLER_WHOIS_ENABLED must be set to True before "
+        "setting OPENWISP_CONTROLLER_ESTIMATED_LOCATION_ENABLED to True."
+    )
