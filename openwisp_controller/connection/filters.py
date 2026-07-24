13c710b3930d79a4a3e4ab290f9f3a2d842519a0
dee077 <deepanshu.sahu27@gmail.com>
2026-07-24 19:15:08 +0530

    [fix] Add filters

--- openwisp_controller/connection/filters.py
+++ openwisp_controller/connection/filters.py
@@ -1,4 +1,6 @@
+from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+from swapper import load_model
 
 from openwisp_users.multitenancy import MultitenantRelatedOrgFilter
 
@@ -13,3 +15,22 @@ class LocationFilter(MultitenantRelatedOrgFilter):
     field_name = "location"
     parameter_name = "location_id"
     title = _("location")
+
+
+class TypeFilter(admin.SimpleListFilter):
+    title = _("type")
+    parameter_name = "type"
+
+    def lookups(self, request, model_admin):
+        BatchCommand = load_model("connection", "BatchCommand")
+        qs = BatchCommand.objects.all()
+        if not request.user.is_superuser:
+            qs = qs.filter(organization_id__in=request.user.organizations_managed)
+        types = qs.values_list("type", flat=True).distinct()
+        choices = dict(BatchCommand._meta.get_field("type").choices)
+        return [(t, choices.get(t, t)) for t in types]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(type=self.value())
+        return queryset
